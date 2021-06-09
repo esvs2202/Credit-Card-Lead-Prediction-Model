c1ed913f0d1a24cac76c3aca8403e539c938b69a
Sriram E.S.V <sriramesv96@gmail.com>
2021-06-09 20:15:05 +0530

    Delete request.py

--- request.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = 'http://127.0.0.1:5000/predict_api'
-
-r = requests.post(url, json={'Gender': 'Male', 'Age': 25, 'Region Code': 'RG250', 'Occupation': 'Salaried', 'Channel Code': 'X1',
-                            'Vintage period in months': 36, 'Having any Credit Product ?': 'Yes', 'Average Account Balance': 100000,
-                            'Is an active customer ?': 'Yes'})
-
-print(r.json())
